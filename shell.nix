6e1b4f9902fa658bb0c7f6f52a3d64dbfd727ae8
jakobrs <jakobrs100@gmail.com>
2020-04-10 11:24:29 +0200

    Explicitly import nur in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }: with pkgs;
+{ pkgs ? import <nixpkgs> {}
+, nur  ? import (fetchTarball https://github.com/nix-community/nur/archive/master.tar.gz) { inherit pkgs; }
+}: with pkgs;
 
 mkShell {
   buildInputs = [ (python3.withPackages (p: with p; [ flask waitress nur.repos.jakobrs.mcstatus ])) ];
