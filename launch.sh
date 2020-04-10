94465d9585f06c63285c0fc3c0b0158fc2596bb8
jakobrs <jakobrs100@gmail.com>
2020-04-10 09:50:34 +0200

    Stylistic changes in launch.sh

--- launch.sh
+++ launch.sh
@@ -3,4 +3,7 @@
 
 echo Starting
 
-waitress-serve --port 8081 --trusted-proxy-headers="x-forwarded-for x-forwarded-host x-forwarded-proto x-forwarded-port" --trusted-proxy="127.0.0.1" 'mcstatus_aas:app'
+waitress-serve \
+  --listen "127.0.0.1:8081" \
+  --trusted-proxy "127.0.0.1" --trusted-proxy-headers "x-forwarded-for x-forwarded-host x-forwarded-proto x-forwarded-port" \
+  'mcstatus_aas:app'
